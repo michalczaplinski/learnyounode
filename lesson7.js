59859c761be3e3a960389edd614b25dfefcb4a61
Michal Czaplinski <michal.czaplinski@gxp-brain.com>
2015-04-28 14:41:57 +0200

    init

---
+++ lesson7.js
@@ -0,0 +1,17 @@
+http = require('http')
+
+url = process.argv[2]
+
+var data = http.get(url, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on("error", function (error) {
+        console.error('There was an error:', err)
+    })
+
+    response.on("data", function (data) {
+        console.log(data)
+
+    })
+})
