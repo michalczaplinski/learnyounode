913dc26057b5dc28e06048af7d79b6b45af0e64d
Michal Czaplinski <michal.czaplinski@gxp-brain.com>
2015-04-28 15:18:45 +0200

    lesson8 completed in flying colors

--- lesson7.js
+++ lesson7.js
@@ -4,14 +4,14 @@ url = process.argv[2]
 
 var data = http.get(url, function (response) {
 
-    response.setEncoding('utf8');
+    // response.setEncoding('utf8');
 
     response.on("error", function (error) {
         console.error('There was an error:', err)
     })
 
     response.on("data", function (data) {
-        console.log(data)
+        console.log(data.toString())
 
     })
 })
