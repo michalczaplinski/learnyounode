972870a05baa7efc5cdfa2fa801f72418550330e
Michal Czaplinski <michal.czaplinski@gxp-brain.com>
2015-04-28 15:19:45 +0200

    NOW added lesson 8

---
+++ lesson8.js
@@ -0,0 +1,23 @@
+http = require('http')
+
+url = process.argv[2]
+
+var data = http.get(url, function (response) {
+
+    var stream = ''
+
+    // response.setEncoding('utf8')
+
+    response.on("error", function (error) {
+        console.error('There was an error:', err)
+    })
+
+    response.on("data", function (data) {
+        stream = stream + data.toString();
+    })
+
+    response.on("end", function () {
+        console.log(stream.length);
+        console.log(stream);
+    })
+})
